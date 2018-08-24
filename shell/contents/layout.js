be75447710b3874d227d0fa68b41b1afdc9fc691
Marco Martin <notmart@gmail.com>
2018-08-24 15:39:41 +0200

    slideshow by default

--- shell/contents/layout.js
+++ shell/contents/layout.js
@@ -1,7 +1,12 @@
 
 var desktopsArray = desktopsForActivity(currentActivity());
 for (var j = 0; j < desktopsArray.length; j++) {
-    desktopsArray[j].wallpaperPlugin = "org.kde.image";
-    desktopsArray[j].addWidget("org.kde.plasma.analogclock");
-    desktopsArray[j].addWidget("org.kde.plasma.mycroftplasmoid");
+    var desk = desktopsArray[j];
+    desk.wallpaperPlugin = "org.kde.slideshow";
+    desk.addWidget("org.kde.plasma.analogclock");
+    desk.addWidget("org.kde.plasma.mycroftplasmoid");
+
+    desk.currentConfigGroup = new Array("Wallpaper","org.kde.slideshow","General");
+    desk.writeConfig("SlideInterval", 480);
+    desk.writeConfig("SlidePaths", "/usr/share/wallpapers/");
 }
