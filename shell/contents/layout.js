214b3cf58fd23326cf553f86c1d78d1da88c3d49
Marco Martin <notmart@gmail.com>
2018-09-03 18:24:10 +0200

    default to new containment

--- shell/contents/layout.js
+++ shell/contents/layout.js
@@ -3,8 +3,8 @@ var desktopsArray = desktopsForActivity(currentActivity());
 for (var j = 0; j < desktopsArray.length; j++) {
     var desk = desktopsArray[j];
     desk.wallpaperPlugin = "org.kde.slideshow";
-    desk.addWidget("org.kde.plasma.analogclock");
-    desk.addWidget("org.kde.plasma.mycroftplasmoid");
+    desk.addWidget("org.kde.plasma.digitalclock");
+//    desk.addWidget("org.kde.plasma.mycroftplasmoid");
 
     desk.currentConfigGroup = new Array("Wallpaper","org.kde.slideshow","General");
     desk.writeConfig("SlideInterval", 480);
