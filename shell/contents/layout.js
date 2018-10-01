8986e3223b883a5935ad5b5056742b30f21dd7ee
Kai Uwe Broulik <kde@privat.broulik.de>
2018-10-01 13:54:28 +0200

    Use org.kde.mycroft.launcher plasmoid instead of hardcoded quicklaunch

--- shell/contents/layout.js
+++ shell/contents/layout.js
@@ -4,10 +4,8 @@ for (var j = 0; j < desktopsArray.length; j++) {
     var desk = desktopsArray[j];
     desk.wallpaperPlugin = "org.kde.slideshow";
     desk.addWidget("org.kde.plasma.digitalclock");
-    var quicklaunch = desk.addWidget("org.kde.plasma.quicklaunch");
-    quicklaunch.writeConfig("launcherUrls", ["file:///usr/share/applications/net.bs.mycroft.gui.desktop", "file:///usr/share/applications/net.bs.mycroft.gui.device3.desktop", "file:///usr/share/applications/net.bs.mycroft.gui.device.desktop", "file:///usr/share/applications/net.bs.mycroft.gui.device2.desktop", "file:///usr/share/applications/net.bs.mycroft.installer.desktop", "file:///usr/share/applications/org.kde.konsole.desktop"]);
-    quicklaunch.writeConfig("maxSectionCount", "2");
-    quicklaunch.writeConfig("showLauncherNames", "true");
+    desk.addWidget("org.kde.mycroft.launcher");
+
 //    desk.addWidget("org.kde.plasma.mycroftplasmoid");
 
     desk.currentConfigGroup = new Array("Wallpaper","org.kde.slideshow","General");
